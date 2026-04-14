a276f1b19e70b830e8b070cb6dfefd5ae3176f5e
Jorokumo <shiro8kumo@gmail.com>
2026-04-13 17:18:01 -0700

    Removed Neovim Config for some problems

--- home/kumo.nix
+++ home/kumo.nix
@@ -2,18 +2,12 @@
 
 {
 
-    imports = [
-	      ../modules/home/neovim.nix
-    ];
-
     # ------- Version ------- #
     home.stateVersion = "25.11";
 
     home.username = username;
     home.homeDirectory = "/home/${username}";
 
-    # ------- init lua ------- #
-    xdg.configFile."nvim/init.lua".source = ../nvim/init.lua;
 
     # ------- User Only Packages ------- #
     home.packages = with pkgs; [
